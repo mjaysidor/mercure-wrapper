5e6ded6c70927085b7642e7eb98a55d7d9ee3868
mjsidor <xxplod3r>
2021-06-30 11:40:08 +0200

    refactor: move server address to .env

--- start.sh
+++ start.sh
@@ -7,8 +7,10 @@ fi
 
 publisher_jwt=$(cat .env.local | grep MERCURE_PUBLISHER_JWT_KEY | cut -d '=' -f2)
 subscriber_jwt=$(cat .env.local | grep MERCURE_SUBSCRIBER_JWT_KEY | cut -d '=' -f2)
+server_name=$(cat .env.local | grep SERVER_NAME | cut -d '=' -f2)
 
-SERVER_NAME=:3000 \
+SERVER_NAME=$server_name \
+MERCURE_PUBLISHER_JWT_KEY=$publisher_jwt \
 MERCURE_PUBLISHER_JWT_KEY=$publisher_jwt \
 MERCURE_SUBSCRIBER_JWT_KEY=$subscriber_jwt \
 ./mercure run -config $caddyfile
